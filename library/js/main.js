ba9599228300d7a9ff43d07bc740854f577183b4
jamesabels <jamesrabels@gmail.com>
2014-09-05 07:34:08 -0400

    Added a mario render test and some finishing touches

--- library/js/main.js
+++ library/js/main.js
@@ -1,11 +1,12 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-stage', { preload: preload, create: create, update: update });
 
 function preload() {
+    game.load.image('renderTest', 'library/assets/mario.png');
 
 }
 
 function create() {
-    
+    game.add.sprite(400 - 32, 300 - 32, 'renderTest');
 }
 
 function update() {
