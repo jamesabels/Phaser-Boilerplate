a3478c478ff59a168cdc8980fa4f1a992503a549
James Abels <jamesrabels@gmail.com>
2014-11-07 13:08:40 -0500

    Changed to mega man image for 1.0 release

--- library/js/states/preload.js
+++ library/js/states/preload.js
@@ -6,7 +6,7 @@ PhaserBp.Preload = function(game) {
 PhaserBp.Preload.prototype = {
 
 	preload: function () {
-    this.load.image('renderTest', 'library/assets/images/mario.png');
+    this.load.image('renderTest', 'library/assets/images/megaman.png');
 	},
 
 	create: function () {
